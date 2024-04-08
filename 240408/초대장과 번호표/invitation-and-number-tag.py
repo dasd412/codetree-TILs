4d95ca95e4aba8352d233ef66a85911bfdc3ab03
jun <dasd412@naver.com>
2024-04-08 12:33:09 +0900

    Title: 초대장과 번호표, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240408/초대장과 번호표/invitation-and-number-tag.py
+++ 240408/초대장과 번호표/invitation-and-number-tag.py
@@ -2,14 +2,19 @@ n,g = list(map(int, input().split()))
 
 arr=[]
 
+person=set()
+
 for i in range(g):
     li= list(map(int, input().split()))
+    if li[0]==2 and 1 in li[1:]:
+        person.add(li[1])
+        person.add(li[2])
+
     arr.append(li[1:])
 
 arr.sort(key=lambda x : len(x))
 
-person={1}
-
+# O(GN)
 for li in arr:
 
     temp=set()
@@ -20,14 +25,5 @@ for li in arr:
     if len(temp-person)==1:
         person=person|temp
 
-# # 1로 시작하지 않는 경우도 포함하기 위해 한 번 더 해야함.
-for li in arr:
-
-    temp=set()
-    for i in range(len(li)):
-        temp.add(li[i])
-
-    if len(temp-person)==1:
-        person=person|temp
 
 print(len(person))
