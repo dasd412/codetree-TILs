648d8bf9cb7d286272c2f8b9648d0d5ae140976f
jun <dasd412@naver.com>
2024-04-10 12:08:41 +0900

    Title: 차이가 가장 작은 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240410/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
+++ 240410/차이가 가장 작은 수/the-number-with-the-smallest-difference.py
@@ -1,32 +1,24 @@
-from sortedcontainers import SortedSet
 import sys
+from sortedcontainers import SortedSet
 
-ss=SortedSet()
 
 n,m=list(map(int, input().split()))
 
+ss=SortedSet()
 for i in range(n):
     num=int(input())
     ss.add(num)
 
-# sorted_set은 오름차순으로 정렬되있음을 이용한다.
-# left ~ right 투포인터를 활용.
-
-left=0
-right=len(ss)-1
-
 answer=sys.maxsize
 
-while left<=right:
-    if abs(ss[left]-ss[right])>=m:
-        answer=min(answer,abs(ss[left]-ss[right]))
+for i in range(n):
+    # 차이가 m 이상 나는 것 중 가장 첫 번째로 만나는 것을 택한다. 
+    index=ss.bisect_right(ss[-1-i]-m)-1
 
-    if left+1<right and abs(ss[left+1]-ss[right])>=m:
-        left+=1
-    elif right-1>left and abs(ss[left]-ss[right-1])>=m:
-        right-=1
-    else:
-        break
+    if ss[-1-i]-ss[index]>=m:
+        answer=min(answer,ss[-1-i]-ss[index])
+        if ss[-1-i]-ss[index]==m:
+            break
 
 
 if answer==sys.maxsize:
