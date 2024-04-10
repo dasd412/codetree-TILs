ec7eefc3289d9e2a73b2936a98870ba5b6ef2636
jun <dasd412@naver.com>
2024-04-10 11:11:28 +0900

    Title: 자리 차지하기, Time: 1065ms, Memory: 37MB, Status: Passed - CodeTree

--- 240410/자리 차지하기/take-place.py
+++ 240410/자리 차지하기/take-place.py
@@ -10,14 +10,20 @@ for i in range(1,m+1):
 
 answer=0
 
+max_val=m
+
 # 최대한 많이 앉힐려면, 앞자리부터 최대한 번호가 높은 것을 택해야 한다.
 # 시간 복잡도는 O(N log M)
 for elem in arr:
     if len(ss)==0:
         break
     index=ss.bisect_right(elem)-1
+    
+    # 자리에 앉을 수 있는지 확인한다.
     if ss[index]<=elem:
         answer+=1
         ss.remove(ss[index])
+    else:
+        break
 
 print(answer)
