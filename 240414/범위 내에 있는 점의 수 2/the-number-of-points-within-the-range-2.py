a1e47e43d0763424016c1759df3f79b65b776cd3
jun <dasd412@naver.com>
2024-04-14 12:51:58 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 553ms, Memory: 41MB, Status: Passed - CodeTree

--- 240414/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 240414/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -1,14 +1,14 @@
 n,q = list(map(int, input().split()))
 
-area=[0]* 1000001
+area=[0]* 1000002
 
 arr = list(map(int, input().split()))
 for elem in arr:
-    area[elem]=1
+    area[elem+1]=1
 
 for i in range(1,len(area)):
     area[i]+=area[i-1]
 
 for _ in range(q):
     a,b = list(map(int, input().split()))
-    print(area[b]-area[a-1])
+    print(area[b+1]-area[a])
