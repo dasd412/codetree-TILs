92ea37212e2246bb7cf58c0025c7d36d0f3b3478
jun <dasd412@naver.com>
2024-04-14 12:31:29 +0900

    Title: 연속한 K개의 숫자, Time: 157ms, Memory: 26MB, Status: Passed - CodeTree

--- 240414/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 240414/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -1,22 +1,21 @@
-import heapq
 import sys
 
 n,k,b= list(map(int, input().split()))
 
 # 누적합의 경우 구간의 누적된 합을 빠르게 구할 때도 사용하지만
 # 지금까지 나온 개수들을 누적할 때도 사용한다.
-heap=[]
+sets=set()
 
 for i in range(b):
     x=int(input())
-    heapq.heappush(heap,x)
+    sets.add(x)
 
 prefix_sum=[0]*(n+1)
 
 val=0
 for i in range(1, n+1):
-    if heap[0]==i:
-        heapq.heappop(heap)
+    if i in sets:
+        sets.remove(i)
         val+=1
     prefix_sum[i]=val
 
