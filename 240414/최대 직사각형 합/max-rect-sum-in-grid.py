a0024a47cb61ed905530e15c3a875f61837e7d24
jun <dasd412@naver.com>
2024-04-14 14:12:05 +0900

    Title: 최대 직사각형 합, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240414/최대 직사각형 합/max-rect-sum-in-grid.py
+++ 240414/최대 직사각형 합/max-rect-sum-in-grid.py
@@ -9,22 +9,29 @@ for i in range(n):
     for j in range(n):
         arr[i][j]=split[j]
 
-prefix_sum=[[0]*(n+1) for _ in range(n+1)]
+dp=[[0]*(n+1) for _ in range(n+1)]
 
 # O(n^2)=O(90000)
 for i in range(1,n+1):
     for j in range(1,n+1):
-        prefix_sum[i][j]=prefix_sum[i][j-1]+prefix_sum[i-1][j]-prefix_sum[i-1][j-1]+arr[i-1][j-1]
+        dp[i][j]=dp[i][j-1]+dp[i-1][j]-dp[i-1][j-1]+arr[i-1][j-1]
 
 answer=-sys.maxsize
 
-# (i,j)를 직사각형 범위라 한다.
-# O(n^4)이므로 시간초과일듯?
+# (i,j)를 좌표, k를 길이라고 한다.
+# O(N^3)=O(300^3) ~ O(10^6)이므로 충분할 듯.
 for i in range(1,n+1):
     for j in range(1,n+1):
-        for k in range(i,n+1):
-            for l in range(j,n+1):
-                val=prefix_sum[k][l]-prefix_sum[k-i][l]-prefix_sum[k][l-j]+prefix_sum[k-i][l-j]
-                answer=max(answer,val)
+        for k in range(1,max(i,j)+1):
+            # 정사각형
+            cand1=dp[i][j]-dp[i][j-k]-dp[i-k][j]+dp[i-k][j-k] if i-k>=0 and j-k>=0 else 0
+            
+            # 세로
+            cand2=dp[i][j]-dp[i-k][j]-dp[i][j-1]+dp[i-k][j-1] if i-k>=0 and j-1>=0 else 0
 
+            # 가로
+            cand3=dp[i][j]-dp[i][j-k]-dp[i-1][j]+dp[i-1][j-k] if j-k>=0 and i-1>=0 else 0
+
+            answer=max(answer,cand1,cand2,cand3)
+            
 print(answer)
