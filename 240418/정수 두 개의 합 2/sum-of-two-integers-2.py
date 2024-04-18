55d3a7772a76e2a6c5b9956334ca4fc4aa9a8c74
jun <dasd412@naver.com>
2024-04-18 11:21:49 +0900

    Title: 정수 두 개의 합 2, Time: 5076ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

---
+++ 240418/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -0,0 +1,28 @@
+n,k=list(map(int,input().split()))
+
+arr=[]
+for i in range(n):
+    num=int(input())
+    arr.append(num)
+
+arr.sort()
+
+# 제일 범위가 넓은 구간을 찾는다.
+left=0
+right=0
+for i in range(len(arr)):
+    if arr[left]+arr[i]<=k:
+        right=i
+
+answer=0
+while left<right:
+    # 정렬되어있으므로 맨 왼쪽 요소와 맨 오른쪽 요소를 합해서 k이하가 되면, 그 중간에 있는 것들도 모두 가능하다.
+    if arr[left]+arr[right]<=k:
+        answer+=(right-left)
+
+        if arr[left]+arr[right]==k:
+            right-=1
+        left+=1
+        
+    
+print(answer)
