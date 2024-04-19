d87f41009a721191576ae26b0bc25996f577ab52
jun <dasd412@naver.com>
2024-04-19 10:04:16 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 151ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240419/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
+++ 240419/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.py
@@ -13,16 +13,19 @@ for i in range(n):
 arr.sort(key=lambda x:x[1])
 
 left=0
-right=len(arr)-1
+right=1
 
 answer=sys.maxsize
 
 # B-A는 구간인데, y좌표 기준으로 오름차순 정렬해서 y기준으로 범위를 좁혀보자. 그 때, x는 대소 비교를 할 필요 없다. 절댓값 씌우면 되지 않을까
-while left<right:
-    if arr[left][1]+arr[right][1]>=d:
+while right<n:
+    if abs(arr[left][1]-arr[right][1])>=d:
         answer=min(answer,abs(arr[right][0]-arr[left][0]))
-        right-=1
-    else:
         left+=1
+    else:
+        right+=1
 
-print(answer)
+if answer==sys.maxsize:
+    print(-1)
+else:
+    print(answer)
