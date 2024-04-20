5003d3aaedb6b6ab591d407f3856787c3d533d6d
jun <dasd412@naver.com>
2024-04-20 13:56:45 +0900

    Title: 가까운 숫자, Time: 1652ms, Memory: 39MB, Status: Passed - CodeTree

--- 240420/가까운 숫자/nearest-number.py
+++ 240420/가까운 숫자/nearest-number.py
@@ -7,14 +7,17 @@ n=int(input())
 arr = list(map(int, input().split()))
 ss.add(0)
 
+answer=sys.maxsize
 for elem in arr:
+
     ss.add(elem)
 
-    answer=sys.maxsize
+    right=ss.bisect_right(elem)
+    left=ss.bisect_left(elem)-1
 
-    for x in ss:
-        index=ss.bisect_right(x)
-        if index<len(ss):
-            answer=min(answer,ss[index]-x)
+    right_length=ss[right]-elem if right<len(ss) else sys.maxsize
+    left_length=elem-ss[left] if 0<=left<len(ss) else sys.maxsize
+
+    answer=min(answer,left_length,right_length)
 
     print(answer)
