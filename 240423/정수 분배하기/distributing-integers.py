c4394ffae90ff0194585b344f817ba3220c2dd9e
jun <dasd412@naver.com>
2024-04-23 11:36:58 +0900

    Title: 정수 분배하기, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240423/정수 분배하기/distributing-integers.py
@@ -0,0 +1,28 @@
+n,m=list(map(int,input().split()))
+
+arr=[]
+
+for i in range(n):
+    num=int(input())
+    arr.append(num)
+
+start=1
+end=min(arr)
+
+answer=0
+
+#그래프를 그려보면 단조 감소 형태를 띈다.
+while start<=end:
+    mid=(start+end)//2
+
+    count=0
+    for elem in arr:
+        count+=(elem//mid)
+
+    if count>=m:
+        answer=max(answer,mid)
+        start=mid+1
+    else:
+        end=mid-1
+
+print(answer)
