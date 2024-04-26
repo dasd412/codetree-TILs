af796ec7d4d00f492e348c88f0beba265b74bd8d
jun <dasd412@naver.com>
2024-04-26 12:14:41 +0900

    Title: 연속한 K개의 숫자, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240426/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 240426/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -3,28 +3,30 @@ import sys
 n,k,b=list(map(int, input().split()))
 
 arr=[i+1 for i in range(n)]
-
 removed=set()
-
 for i in range(b):
     num=int(input())
     removed.add(num)
 
 answer=sys.maxsize    
 
-for left in range(n):
+"""
+투 포인터 풀이보단 누적합 풀이가 더 효율적이다.
+"""
 
-    count=0
+prefix_sum=[0]*(n+1)
 
-    right=left
-    while right<n and right-left<k:
-        if arr[right] in removed:
-            count+=1
-        right+=1
+for i in range(1,n+1):
+    if arr[i-1] in removed:
+        prefix_sum[i]+=1
+    
+for i in range(1,n+1):
+    prefix_sum[i]+=prefix_sum[i-1]
 
-    answer=min(answer,count)
-
-    if n-left<=k:
+for left in range(1,n+1):
+    right=left+k-1
+    if right>=n:
         break
+    answer=min(answer,prefix_sum[right]-prefix_sum[left-1])
 
 print(answer)
