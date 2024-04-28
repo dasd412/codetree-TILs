5f322eff10389c796b71c68dad5a0a543547f800
jun <dasd412@naver.com>
2024-04-28 16:17:40 +0900

    Title: 정원 입장은 선착순, Time: 851ms, Memory: 38MB, Status: Passed - CodeTree

--- 240428/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
+++ 240428/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -2,19 +2,20 @@ import heapq
 
 n=int(input())
 
+# (a,i,t) 순. 시작 시간이 가장 빠른 순서대로 넣는다. 만약 시작 시간이 같을 경우엔 번호순이 작은 순서다.
 times=[]
 
 for i in range(n):
     a,t=list(map(int, input().split()))
-    heapq.heappush(times,(a,t,i))
+    heapq.heappush(times,(a,i,t))
 
 
 answer=0
 
-# 대기하고 있는 사람 우선 순위 큐. 번호표 순 (a,t,i)-> (i,a,t)
+# 대기하고 있는 사람 우선 순위 큐. 번호표 순 (a,i,t)-> (i,a,t)
 wait_queue=[]
 
-start,wait,number=heapq.heappop(times)
+start,number,wait=heapq.heappop(times)
 
 current_time=start+wait
 
@@ -22,7 +23,7 @@ while len(times) or len(wait_queue):
     
     while len(times):
         if times[0][0]<=current_time:
-            start,wait,number=heapq.heappop(times)
+            start,number,wait=heapq.heappop(times)
             heapq.heappush(wait_queue,(number,start,wait))
         else:
             break
@@ -32,7 +33,7 @@ while len(times) or len(wait_queue):
         answer=max(answer,current_time-a)
         current_time+=t
     else:
-        a,t,i=heapq.heappop(times)
+        a,i,t=heapq.heappop(times)
         current_time=a+t
 
 print(answer)
